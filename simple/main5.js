01dc65237f109b21a7dedd69a628c67eb6f20d92
briandanilo <brian.danilo@gmail.com>
2017-03-11 11:28:27 -0600

    table working

--- simple/main5.js
+++ simple/main5.js
@@ -2,19 +2,22 @@
 // });
 
 
+
 document.querySelector('#convert-btn').addEventListener("click",function(e){
 
   e.preventDefault();
+  tableObject=[];
+  clearTable();
 
   var away = document.querySelector('#away-odds').value;
   var home = document.querySelector('#home-odds').value;
 
-  var awayImplied = convert(away,home,"awayImplied");
-  var homeImplied = convert(away,home,"homeImplied");
+  var awayImplied = convert(away,home,"awayImplied").toFixed(2);
+  var homeImplied = convert(away,home,"homeImplied").toFixed(2);
   var awayUsaWithJuice = convert(away,home,"awayUsaWithJuice");
   var homeUsaWithJuice = convert(away,home,"homeUsaWithJuice");
-  var awayDecimalWithJuice = convert(away,home,"awayDecimalWithJuice");
-  var homeDecimalWithJuice = convert(away,home,"homeDecimalWithJuice");
+  var awayDecimalWithJuice = convert(away,home,"awayDecimalWithJuice").toFixed(2);
+  var homeDecimalWithJuice = convert(away,home,"homeDecimalWithJuice").toFixed(2);
   var awayFractionalWithJuice = convert(away,home,"awayFractionalWithJuice");
   var homeFractionalWithJuice = convert(away,home,"homeFractionalWithJuice");
   var awayUsaNoJuice = convert(away,home,"awayUsaNoJuice");
@@ -24,8 +27,33 @@ document.querySelector('#convert-btn').addEventListener("click",function(e){
   var awayFractionalNoJuice = convert(away,home,"awayFractionalNoJuice");
   var homeFractionalNoJuice = convert(away,home,"homeFractionalNoJuice");
 
-
-
+  var tableObject = [
+    {
+    "name":"Implied Probability",
+    "away":awayImplied,
+    "home":homeImplied,
+  },
+    {
+    "name":"American (with vig)",
+    "away":awayUsaWithJuice,
+    "home":homeUsaWithJuice,
+  },
+    {
+    "name":"Decimal (with vig)",
+    "away":awayDecimalWithJuice,
+    "home":homeDecimalWithJuice,
+  },
+    {
+    "name":"American (no vig)",
+    "away":awayUsaNoJuice,
+    "home":homeUsaNoJuice,
+  },
+    {
+    "name":"Decimal (no vig)",
+    "away":awayDecimalNoJuice,
+    "home":homeDecimalNoJuice,
+    }
+  ]
 
   var table = document.querySelector('.table');
   var header = table.createTHead();
@@ -40,30 +68,29 @@ document.querySelector('#convert-btn').addEventListener("click",function(e){
   cell1.innerHTML = "<b>Away Team</b>";
   cell2.innerHTML = "<b>Home Team</b>";
 
-  var row = table.insertRow(-1);
 
-  var cell0 = row.insertCell(0);
-  var cell1 = row.insertCell(1);
-  var cell2 = row.insertCell(2);
-
-  cell0.innerHTML = "Implied Probability";
-  cell1.innerHTML = awayImplied.toFixed(2);
-  cell2.innerHTML = homeImplied.toFixed(2);
-
-  var row = table.insertRow(-1);
-
-  var cell0 = row.insertCell(0);
-  var cell1 = row.insertCell(1);
-  var cell2 = row.insertCell(2);
-
-  cell0.innerHTML = "No Vig (American)";
-  cell1.innerHTML = awayUsaNoJuice;
-  cell2.innerHTML = homeUsaNoJuice;
-
+  for (i in tableObject){
+    var row = table.insertRow(-1);
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    cell0.innerHTML = tableObject[i].name;
+    cell1.innerHTML = tableObject[i].away;
+    cell2.innerHTML = tableObject[i].home;
+  }
 
 });
 
 
+function clearTable(){
+
+  if (document.querySelector('.table')){
+    var table = document.querySelector('.table')
+    while(table.rows.length > 0) {
+      table.deleteRow(0);
+    }
+  }
+}
 
 
 
@@ -73,6 +100,13 @@ function convert(awayLine,homeLine,lineType) {
       //get implied prob with vig
       var homeImpliedProb = getImpliedProb(awayLine);
       var awayImpliedProb = getImpliedProb(homeLine);
+
+      var awayUsaWithJuice = impliedToAmerican(awayImpliedProb);
+      var homeUsaWithJuice = impliedToAmerican(homeImpliedProb);
+
+      var awayDecimalWithJuice = impliedToDecimal(awayImpliedProb);
+      var homeDecimalWithJuice = impliedToDecimal(homeImpliedProb);
+
 
       //calculate implied prob without vig
       var noVigAwayImplied = awayImpliedProb / (homeImpliedProb + awayImpliedProb)
@@ -105,18 +139,18 @@ function convert(awayLine,homeLine,lineType) {
       switch(lineType){
         case "awayImplied": return noVigAwayImplied;
         case "homeImplied": return noVigHomeImplied;
-        //case "awayUsaWithJuice": return awayUsaWithJuice; break;
-        //case "homeUsaWithJuice": return homeUsaWithJuice;
-        //case "awayDecimalWithJuice": return awayDecimalWithJuice;
-        //case "homeDecimalWithJuice": return homeDecimalWithJuice;
-        //case "awayFractionalWithJuice": return awayFractionalWithJuice;
-        //case "homeFractionalWithJuice": return homeFractionalWithJuice;
+        case "awayUsaWithJuice": return awayUsaWithJuice;
+        case "homeUsaWithJuice": return homeUsaWithJuice;
+        case "awayDecimalWithJuice": return awayDecimalWithJuice;
+        case "homeDecimalWithJuice": return homeDecimalWithJuice;
+        // case "awayFractionalWithJuice": return awayFractionalWithJuice;
+        // case "homeFractionalWithJuice": return homeFractionalWithJuice;
         case "awayUsaNoJuice": return noVigAwayLineUSA;
         case "homeUsaNoJuice": return noVigHomeLineUSA;
         case "awayDecimalNoJuice": return noVigAwayLineDecimal;
         case "homeDecimalNoJuice": return noVigAwayLineDecimal;
-        case "awayFractionalNoJuice": return noVigAwayLineFractional;
-        case "homeFractionalNoJuice": return noVigHomeLineFractional;
+        // case "awayFractionalNoJuice": return noVigAwayLineFractional;
+        // case "homeFractionalNoJuice": return noVigHomeLineFractional;
       }
 
 }
